588928883ca6e855a8648a33baee763ec969cb06
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-27 17:12:22 +0600

    add assets file

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+;(function($){
+    'use strict';
+    const doc = $(document);
+
+    class GetRestPosts{
+        constructor(){
+            // do something
+        }
+    }
+
+    doc.ready(()=>{new GetRestPosts()});
+    
+})(jQuery);

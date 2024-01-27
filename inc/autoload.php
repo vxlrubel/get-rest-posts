4109fe354c4a9dda151eb297b5d2fe892567caef
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-27 17:15:22 +0600

    create autoload file

---
+++ inc/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+// directly access denied
+defined('ABSPATH') || exit;
+
+$files = [
+    'classes/class-assets',
+];
+
+foreach ( $files as $file ) {
+    if( file_exists(  dirname( __FILE__ ) . '/' . $file . '.php' ) ){
+        require_once dirname( __FILE__ ) . '/' . $file . '.php';
+    }else{
+        $not_found = dirname(__FILE__) . '/' . $file . '.php';
+        throw new Exception("file not found {$not_found}", 1);
+    }
+}

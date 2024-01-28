a3fe038a065a2b8bf1df938a5ce74aee3718399d
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-28 18:35:10 +0600

    update form fields

--- inc/classes/class-admin-menu.php
+++ inc/classes/class-admin-menu.php
@@ -36,11 +36,31 @@ defined('ABSPATH') || exit;
     }
 
     /**
+     * update form fields
+     *
+     * @return void
+     */
+    private function update_form_fields(){
+        if ( isset( $_REQUEST['set_title'] ) && ! empty( $_REQUEST['set_title'] ) && isset( $_REQUEST['post_per_page'] ) && ! empty( $_REQUEST['post_per_page'] ) ){
+            $title = sanitize_text_field( esc_sql( $_REQUEST['set_title'] ) );
+            $count = (int) sanitize_text_field( esc_sql( $_REQUEST['post_per_page'] ) );
+            update_option( '_set_grp_title', $title );
+            update_option( '_set_grp_count', $count );
+
+            printf('<div class="notice notice-success is-dismissible"><p>%s</p></div>', 'Update successfull.' );
+        }
+    }
+
+    /**
      * render page content
      *
      * @return void
      */
     public function render_page_content(){
+        $this->update_form_fields();
+        $action_url = $_SERVER['PHP_SELF'] . '?page=get-rest-posts';
+        $get_title  = get_option( '_set_grp_title', 'Latest Post' );
+        $get_count  = get_option( '_set_grp_count', 10 );
         ?>
         <div class="wrap grp-wrap">
             <h1 class="wp-heading-inline">Get Rest Posts</h1>
@@ -48,18 +68,18 @@ defined('ABSPATH') || exit;
 
             <div class="inner-wrap">
                 <div class="options">
-                    <form action="">
+                    <form action="<?php echo $action_url; ?>" method="POST">
                         <p>
                             <label>
                                 Set the title:
-                                <input type="text" class="widefat" name="set_title">
+                                <input type="text" class="widefat" name="set_title" value="<?php echo esc_attr( $get_title );?>">
                             </label>
                         </p>
                         <div class="counter-parent">
                             <label for="post-per-page">Posts Per Page:</label>
                             <div class="grp-count">
                                 <a href="javascript:void(0)"><span class="dashicons dashicons-minus"></span></a>
-                                <input type="number" min="5" max="50" id="post-per-page" name="post_per_page">
+                                <input type="number" min="5" max="50" id="post-per-page" name="post_per_page" value="<?php echo esc_attr( $get_count ); ?>">
                                 <a href="javascript:void(0)"><span class="dashicons dashicons-plus-alt2"></span></a>
                             </div>
                         </div>

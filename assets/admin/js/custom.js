89fab4fcfc979810517652bb9cbf527299f3c563
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-28 22:59:09 +0600

    implement increment and decrement method in js

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -10,8 +10,24 @@
         counter(){
             $('.grp-count').on('click', 'a', function(e){
                 e.preventDefault();
-                alert( $(this).attr('class') );
-                console.log( $(this) );
+                let _self         = $(this);
+                let _thisClass    = _self.attr('class');
+                let _input        = _self.siblings('input');
+                let inputValue    = _input.val();
+                let inputMinValue = parseInt( _input.attr('min') );
+                let inputMaxValue = parseInt( _input.attr('max') );
+                let inputStep     = parseInt( _input.attr('step') );
+
+                if( _thisClass === 'minus' ){
+                    if ( inputMinValue < inputValue ){
+                        _input.val( parseInt(inputValue, 10) - inputStep );
+                    }
+                }
+                if ( _thisClass === 'plus' ){
+                    if ( inputMaxValue > inputValue ){
+                        _input.val(parseInt(inputValue, 10 ) + inputStep);
+                    }
+                }
             });
         }
     }
